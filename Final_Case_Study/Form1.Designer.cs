76ed1f74ad91f99b57d0e2fd760333a310724537
Alfred D. Paris <gudo.alfred@gmail.com>
2023-06-24 08:01:18 +0800

    ipt final

---
+++ Final_Case_Study/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Final_Case_Study
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.BTN_EXPORT = new System.Windows.Forms.Button();
+            this.BTN_IMPORT = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(450, 276);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(686, 12);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(450, 276);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // BTN_EXPORT
+            // 
+            this.BTN_EXPORT.Location = new System.Drawing.Point(93, 333);
+            this.BTN_EXPORT.Name = "BTN_EXPORT";
+            this.BTN_EXPORT.Size = new System.Drawing.Size(209, 42);
+            this.BTN_EXPORT.TabIndex = 2;
+            this.BTN_EXPORT.Text = "Export";
+            this.BTN_EXPORT.UseVisualStyleBackColor = true;
+            this.BTN_EXPORT.Click += new System.EventHandler(this.BTN_EXPORT_Click);
+            // 
+            // BTN_IMPORT
+            // 
+            this.BTN_IMPORT.Location = new System.Drawing.Point(823, 333);
+            this.BTN_IMPORT.Name = "BTN_IMPORT";
+            this.BTN_IMPORT.Size = new System.Drawing.Size(210, 42);
+            this.BTN_IMPORT.TabIndex = 3;
+            this.BTN_IMPORT.Text = "Import";
+            this.BTN_IMPORT.UseVisualStyleBackColor = true;
+            this.BTN_IMPORT.Click += new System.EventHandler(this.BTN_IMPORT_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(365, 333);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox1.Size = new System.Drawing.Size(307, 30);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "Final Case Study IPT Groupings";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(469, 13);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox2.Size = new System.Drawing.Size(211, 276);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "Group\r\nMembers\r\n\r\nAlfred D. Paris\r\nDiosdado B. De Vega\r\nGeraLdine Frias\r\nGie Ferr" +
+    "er\r\nJasmine De Vera\r\nMichele Ann Fernandez\r\nTheresa Rosario";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1161, 433);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.BTN_IMPORT);
+            this.Controls.Add(this.BTN_EXPORT);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button BTN_EXPORT;
+        private System.Windows.Forms.Button BTN_IMPORT;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Final_Case_Study/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+
+
+
+namespace Final_Case_Study
+{
+    public partial class Form1 : Form
+    {
+        DataTable table = new DataTable("tbl");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            table.Columns.Add("ID", typeof(int));
+            table.Columns.Add("First Name", typeof(string));
+            table.Columns.Add("Last Name", typeof(string));
+            table.Columns.Add("Age", typeof(int));
+
+            table.Rows.Add(1, "First A", "Last A", 10);
+            table.Rows.Add(2, "First B", "Last B", 20);
+            table.Rows.Add(3, "First C", "Last C", 30);
+            table.Rows.Add(4, "First D", "Last D", 40);
+            table.Rows.Add(5, "First E", "Last E", 50);
+            table.Rows.Add(6, "First F", "Last F", 60);
+            table.Rows.Add(7, "First G", "Last G", 70);
+            table.Rows.Add(8, "First H", "Last H", 80);
+            table.Rows.Add(9, "First I", "Last I", 90);
+
+            dataGridView1.DataSource = table;
+        }
+
+        private void BTN_EXPORT_Click(object sender, EventArgs e)
+        {
+            table.WriteXml(@"C:\Users\average_Programmer\Desktop\xml\XMLFile1.xml", XmlWriteMode.WriteSchema);
+            MessageBox.Show("Data Exported");
+        }
+
+        private void BTN_IMPORT_Click(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            dt.ReadXml(@"C:\Users\average_Programmer\Desktop\xml\XMLFile1.xml");
+            dataGridView2.DataSource = dt;
+            MessageBox.Show("Data Imported");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
